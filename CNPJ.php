14c7d12110677a9af4b01163ba39e4feab32c56e
caio-henrique2006 <caio_henrique2006@outlook.com>
2024-06-26 17:57:56 -0300

    add: basic code

---
+++ CNPJ.php
@@ -0,0 +1,17 @@
+<?php
+
+$cnpj = $_REQUEST["data"];
+
+$url = "https://publica.cnpj.ws/cnpj/" . $cnpj;
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+echo $resp;
