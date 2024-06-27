9d8db9fffdc893100b5ba851fa3c8f767d852407
caio-henrique2006 <caio_henrique2006@outlook.com>
2024-06-27 15:33:46 -0300

    add style and time counter

--- script.js
+++ script.js
@@ -1,59 +1,66 @@
+/* public var */
 var empresas = []
 
-function call(obj) {
-  empresas.push(obj);
-  console.log(empresas);
-}
+/* callback */
+function csvBuild () {      
+  var csv = 'cidade, estado, endereço, numero, nome fantasia, cnpj, email, telefone1, telefone2, razao_social\n';
+  empresas.forEach(function(row) {
+          csv += row.estabelecimento.cidade.nome;
+          csv += ','+ row.estabelecimento.estado.nome;
+          csv += ','+ row.estabelecimento.logradouro;
+          csv += ','+ row.estabelecimento.numero;
+          csv += ','+ row.estabelecimento.nome_fantasia;
+          csv += ','+ row.estabelecimento.cnpj;
+          csv += ','+ row.estabelecimento.email;
+          csv += ','+ row.estabelecimento.telefone1;
+          csv += ','+ row.estabelecimento.telefone2;
+          csv += ','+ row.razao_social;
+          csv += '\n';
+  });
+  var hiddenElement = document.createElement('a');
+  hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+  hiddenElement.target = '_blank';
+  hiddenElement.download = 'digiteumnome.csv';
+  hiddenElement.click();
+};
 
+/* CNPJ */
 document.getElementById("cnpj").addEventListener("click", () => {
-  /* AQUI */
-  var cnpj = "2260436400020251526316000152125785180002771342064300013647325487000146204419720002001211419700017934297761000179190510230001531133169600015516883927000238271385890001455330279500015932642852000250546133310001262807677100018123923269000145"
-  
+  document.getElementById("state").innerText = "Iniciando o envio";
   var arr = []
-  for (var i = 0; i < cnpj.length; i += 14) {
+  // Separando os cnpjs:
+  const cnpj = document.getElementById("textarea").value;
+  for (var i = 0; i < cnpj.length; i += 15) {
     arr.push(cnpj.slice(i, i+14));
   }
-  var response = fetchCNPJ(arr, call);
-  console.log("Buscando");
+  // Contador da chegada dos dados:
+  counter = 0;
+  fetchCNPJ(arr, (obj) => {
+    empresas.push(obj);
+    counter += 1;
+    document.getElementById("state").innerText = String(counter) + " de " + arr.length + " buscados";
+    document.getElementById("stateTime").innerText = "Falta: " + Math.round((((21000 * (arr.length - counter))/1000)/60)) + " minutos";
+    if (arr.length == counter) {
+      document.getElementById("state").innerText = "Terminado";
+      document.getElementById("csv").disabled = false;
+    }
+  });
+  console.log(arr);
 })
 
+/* Download CSV */
 document.getElementById("csv").addEventListener("click", () => {
-  
-  var _gerarCsv = function(){
-        
-      var csv = 'cidade, estado, endereço, numero, nome fantasia, cnpj, email, telefone1, telefone2, razao_social\n';
-    
-      empresas.forEach(function(row) {
-              csv += row.estabelecimento.cidade.nome;
-              csv += ','+ row.estabelecimento.estado.nome;
-              csv += ','+ row.estabelecimento.logradouro;
-              csv += ','+ row.estabelecimento.numero;
-              csv += ','+ row.estabelecimento.nome_fantasia;
-              csv += ','+ row.estabelecimento.cnpj;
-              csv += ','+ row.estabelecimento.email;
-              csv += ','+ row.estabelecimento.telefone1;
-              csv += ','+ row.estabelecimento.telefone2;
-              csv += ','+ row.razao_social;
-              csv += '\n';
-      });
-    
-      var hiddenElement = document.createElement('a');
-      hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
-      hiddenElement.target = '_blank';
-      hiddenElement.download = 'produtos.csv';
-      hiddenElement.click();
-  };
-  _gerarCsv();
+  var _gerarCsv = csvBuild();
 })
 
+/* Fetch */
 async function fetchCNPJ(cnpj, callback) {
-    var cont = 1
+    var cont = 0
     var timeout = 0;
     var response = [];
     cnpj.forEach((element) => {
-        timeout += 21000;
+        timeout += 21000; // 21 segundos
         setTimeout(() => {
-            console.log("Opa tempo ", cont);
             cont += 1;
             console.log(cont);
             const xhttp = new XMLHttpRequest();
